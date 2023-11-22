b91144052042cefbb84a70e73aa4a64b85d42fad
neubluk <neubauer.lukas@hotmail.com>
2023-11-22 15:07:17 +0100

    adding fitted values

--- R/avg_fn.R
+++ R/avg_fn.R
@@ -1,31 +1,37 @@
 simple_avg_fn <- function(x, models, neighbors){
-  refit_models <- lapply(models$models[-1], models$refit_fn, ynew=x[[1]][,-1])
-  forecasts <- c(list(models$forecast_fn(models$models[[1]], h=models$h)),
-                 lapply(refit_models,models$forecast_fn,h=models$h))
+  refit_models <- lapply(models$models, models$refit_fn, ynew=x[[1]][,-1])
+  forecasts <- lapply(refit_models, models$forecast_fn, h = models$h)
+  fitted_values <- lapply(refit_models, models$fitted_fn, h = models$h)
 
-  return(Reduce('+', forecasts)/length(forecasts))
+  return(list(forecasts=Reduce('+', forecasts)/length(forecasts),
+              fitted_values=Reduce('+', fitted_values)/length(fitted_values)))
 }
 
 simple_avg_n_fn <- function(x, models, neighbors){
   refit_models <- lapply(models$models[-1], models$refit_fn, ynew=x[[1]][,-1])
   forecasts <- lapply(refit_models,models$forecast_fn,h=models$h)
+  fitted_values <- lapply(refit_models, models$fitted_fn, h = models$h)
 
-  return(Reduce('+', forecasts)/length(forecasts))
+  return(list(forecasts=Reduce('+', forecasts)/length(forecasts),
+              fitted_values=Reduce('+', fitted_values)/length(fitted_values)))
 }
 
 dist_avg_n_fn <- function(x, models, neighbors){
   refit_models <- lapply(models$models[-1], models$refit_fn, ynew=x[[1]][,-1])
   forecasts <- lapply(refit_models,models$forecast_fn,h=models$h)
+  fitted_values <- lapply(refit_models,models$fitted_fn,h=models$h)
   weights <- 1/neighbors$dists
   weights <- weights/sum(weights)
 
-  return(Reduce('+', Map('*', forecasts, weights)))
+  return(list(forecasts=Reduce('+', Map('*', forecasts, weights)),
+              fitted_values=Reduce('+', Map('*', fitted_values, weights))))
 }
 
 dist_avg_fn <- function(x, models, neighbors){
-  refit_models <- lapply(models$models[-1], models$refit_fn, ynew=x[[1]][,-1])
-  forecasts <- c(list(models$forecast_fn(models$models[[1]],h=models$h)),
-                 lapply(refit_models,models$forecast_fn,h=models$h))
+  refit_models <- lapply(models$models, models$refit_fn, ynew=x[[1]][,-1])
+  forecasts <- lapply(refit_models,models$forecast_fn,h=models$h)
+
+  fitted_values <- lapply(refit_models,models$fitted_fn,h=models$h)
 
   init_cent <- neighbors$data[[which.max(lapply(neighbors$data,nrow))]]
   avg <- adba(neighbors$data, init_cent)
@@ -33,7 +39,8 @@ dist_avg_fn <- function(x, models, neighbors){
   weights <- 1/avg$dist_to_avg
   weights <- weights/sum(weights)
 
-  return(Reduce('+', Map('*', forecasts, weights)))
+  return(list(forecasts=Reduce('+', Map('*', forecasts, weights)),
+              fitted_values=Reduce('+', Map('*', fitted_values, weights))))
 }
 
 global_avg_fn <- function(x, models, neighbors){
@@ -41,13 +48,17 @@ global_avg_fn <- function(x, models, neighbors){
   avg <- adba(neighbors$data, init_cent)
   avg_model <- models$model_fn(avg$avg[,-1])
 
-  return(models$forecast_fn(models$refit_fn(avg_model,x[[1]][,-1]), h=models$h))
+  avg_model_refit <- models$refit_fn(avg_model,x[[1]][,-1])
+
+  return(list(forecasts=models$forecast_fn(avg_model_refit, h=models$h),
+              fitted_values=models$fitted_fn(avg_model_refit, h=models$h)))
 }
 
 perf_avg_fn <- function(x, models, neighbors){
-  refit_models <- lapply(models$models[-1], models$refit_fn, ynew=x[[1]][,-1])
-  forecasts <- c(list(models$forecast_fn(models$models[[1]],h=models$h)),
-                 lapply(refit_models,models$forecast_fn,h=models$h))
+  refit_models <- lapply(models$models, models$refit_fn, ynew=x[[1]][,-1])
+  forecasts <- lapply(refit_models,models$forecast_fn,h=models$h)
+
+  fitted_values <- lapply(refit_models,models$fitted_fn,h=models$h)
 
   refit_models_2 <- sapply(seq_along(x), function(i){
     models$refit_fn(models$models[[i]], ynew=x[[i]][,-1])
@@ -60,16 +71,18 @@ perf_avg_fn <- function(x, models, neighbors){
 
   weights <- weights/sum(weights)
 
-  return(Reduce('+', Map('*', forecasts, weights)))
+  return(list(forecasts=Reduce('+', Map('*', forecasts, weights)),
+              fitted_values=Reduce('+', Map('*', fitted_values, weights))))
 
 }
 
 perf_avg_r_fn <- function(x, models, neighbors){
   refit_models <- lapply(models$models, models$refit_fn, ynew=x[[1]][,-1])
   forecasts <- lapply(refit_models,models$forecast_fn,h=models$h)
+  fitted_values <- lapply(refit_models,models$fitted_fn,h=models$h)
 
   weights <- sapply(seq_along(x), function(i) {
-    fitted_vals <- models$fitted_fn(refit_models[[i]],h=models$h)
+    fitted_vals <- fitted_values[[i]]
     if (all(is.na(fitted_vals))) fitted_vals <- matrix(NA, nrow=nrow(x[[1]]), ncol=ncol(x[[1]])-1)
     yhat <- cbind(x[[1]][,1], fitted_vals)
     1/rmsse(x[[1]], yhat)
@@ -77,6 +90,7 @@ perf_avg_r_fn <- function(x, models, neighbors){
 
   weights <- weights/sum(weights)
 
-  return(Reduce('+', Map('*', forecasts, weights)))
+  return(list(forecasts=Reduce('+', Map('*', forecasts, weights)),
+              fitted_values=Reduce('+', Map('*', fitted_values, weights))))
 
 }
